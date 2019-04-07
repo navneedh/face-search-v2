64d44f204c2f1d329343834c2258a4028742c0b6
navneedh <navneedh@gmail.com>
2019-04-06 18:55:15 -0700

    fixed images

--- experiment.py
+++ experiment.py
@@ -14,6 +14,11 @@ import sample as sp
 import util as ut
 
 
+device_name = tf.test.gpu_device_name()
+if device_name != '/device:GPU:0':
+  raise SystemError('GPU device not found')
+print('Found GPU at: {}'.format(device_name))
+
 # Initialize TensorFlow
 tflib.init_tf()
 
