f8fecba19a34b078b2a5f74a4e933f17cf0f46f3
navneedh <navneedh@gmail.com>
2019-05-07 21:10:31 -0700

    minor fix

--- experiment.py
+++ experiment.py
@@ -14,11 +14,6 @@ import time
 from IPython.display import display, Markdown, clear_output
 # widget packages
 import ipywidgets as widgets
-# defining some widgets
-# import sample as sp 
-# import util as ut
-
-
 
 # Initialize TensorFlow
 tflib.init_tf()
@@ -30,27 +25,6 @@ with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
 	# _G = Instantaneous snapshot of the generator. Mainly useful for resuming a previous training run.
 	# _D = Instantaneous snapshot of the discriminator. Mainly useful for resuming a previous training run.
 	# Gs = Long-term average of the generator. Yields higher-quality results than the instantaneous snapshot.
-def create_image_ranking_buttons():
-	image_ranks = []
-	for i in range(6):
-		image_ranks += [widgets.Checkbox(description=str(i+1),)]
-	
-	return image_ranks
-
-def construct_all_buttons():
-	all_ranks = []
-	for i in range(6):
-		all_ranks += [create_image_ranking_buttons()]
-   
-	return all_ranks
-
-def view_buttons():
-	verticalBoxButtons = []
-	buttons = construct_all_buttons()
-	for i in range(6):
-		verticalBoxButtons.append(widgets.VBox(buttons[i]))
-	
-	return widgets.HBox(verticalBoxButtons), buttons 
 
 
 def get_rating_results(buttons):
@@ -103,11 +77,11 @@ def gen_grid_exp(cur_z, exp_iter, experimentNum, original, noise_level = 1):
 	noisyVecs = []
 	noises = []
 	noisyImages = []
-	new_im = Image.new('RGB', (512,64))
+	new_im = Image.new('RGB', (1024,128))
 	index = 0
 	print("Generating grid of noisy images ...")
 	print("      1    2    3    4    5    6")
-	for i in range(0,384,64):
+	for i in range(0,768,128):
 		np.random.seed(np.random.randint(4362634))
 		noise_val = (random_vector() * noise_level) #most noise added
 		zs = cur_z + noise_val
@@ -117,13 +91,15 @@ def gen_grid_exp(cur_z, exp_iter, experimentNum, original, noise_level = 1):
 		noisyVecs.append(zs)
 		noisyImages.append(p_image)
 		im = Image.fromarray(p_image)
-		im.thumbnail((64,64))
+		im.thumbnail((128,128))
 		new_im.paste(im, (i,0))
-		index += 1
+		
+
+	print("SIZE", noisyImages[0].size)
 
 	im = Image.fromarray(original)
-	im.thumbnail((64,64))
-	new_im.paste(im, (448,0))
+	im.thumbnail((128,128))
+	new_im.paste(im, (896,0))
 	index += 1
 		
 	new_im.save("./exp" + str(experimentNum) + "/grid_" +str(exp_iter)+".png")
@@ -133,18 +109,20 @@ def gen_grid_exp(cur_z, exp_iter, experimentNum, original, noise_level = 1):
 	return noisyVecs, noisyImages, noises
 
 def gen_images_to_rank(image_matrices, original, indexToRemove):
-	new_im = Image.new('RGB', ((len(image_matrices) + 1) * 64,64))
+	new_im = Image.new('RGB', ((len(image_matrices) + 1) * 128,128))
 	del image_matrices[indexToRemove - 1]
-	for i in range(0,len(image_matrices) * 64,64):
-		im = Image.fromarray(image_matrices[int(i/64)])
-		im.thumbnail((64,64))
+	for i in range(0,len(image_matrices) * 128,128):
+		im = Image.fromarray(image_matrices[int(i/128)])
+		im.thumbnail((128,128))
 		new_im.paste(im, (i,0))
 	
 	im = Image.fromarray(original)
-	im.thumbnail((64,64))
-	new_im.paste(im, ((len(image_matrices) + 1) * 64,0))
+	im.thumbnail((128,128))
+	new_im.paste(im, ((len(image_matrices) + 1) * 128,0))
 
 	plt.imshow(new_im)
+	plt.grid('off')
+	plt.axis('off')
 	plt.draw()
 	plt.pause(0.001)
 
@@ -157,9 +135,9 @@ def present_noise_choices(cur_z, exp_iter, experimentNum, noise_level = 1):
 	noisyVecs = []
 	noises = []
 	noisyImages = []
-	new_im = Image.new('RGB', (576,64))
+	new_im = Image.new('RGB', (1152,128))
 	index = 0
-	for i in range(0,192,64):
+	for i in range(0,384,128):
 		np.random.seed(np.random.randint(4362634))
 		noise_val = (random_vector() * 0.5) #most noise added
 		zs = cur_z + noise_val
@@ -169,10 +147,10 @@ def present_noise_choices(cur_z, exp_iter, experimentNum, noise_level = 1):
 		noisyVecs.append(zs)
 		noisyImages.append(p_image)
 		im = Image.fromarray(p_image)
-		im.thumbnail((64,64))
+		im.thumbnail((128,128))
 		new_im.paste(im, (i,0))
 		index += 1
-	for i in range(192,384,64):
+	for i in range(384,768,128):
 		np.random.seed(np.random.randint(4362634))
 		noise_val = (random_vector() * 1.3) #most noise added
 		zs = cur_z + noise_val
@@ -182,10 +160,10 @@ def present_noise_choices(cur_z, exp_iter, experimentNum, noise_level = 1):
 		noisyVecs.append(zs)
 		noisyImages.append(p_image)
 		im = Image.fromarray(p_image)
-		im.thumbnail((64,64))
+		im.thumbnail((128,128))
 		new_im.paste(im, (i,0))
 		index += 1
-	for i in range(384,576,64):
+	for i in range(768,1152,128):
 		np.random.seed(np.random.randint(4362634))
 		noise_val = (random_vector() * 8) #most noise added
 		zs = cur_z + noise_val
@@ -195,13 +173,15 @@ def present_noise_choices(cur_z, exp_iter, experimentNum, noise_level = 1):
 		noisyVecs.append(zs)
 		noisyImages.append(p_image)
 		im = Image.fromarray(p_image)
-		im.thumbnail((64,64))
+		im.thumbnail((128,128))
 		new_im.paste(im, (i,0))
 		index += 1
 
 		
 	new_im.save("./exp" + str(experimentNum) + "/grid_" +str(exp_iter)+".png")
 	plt.imshow(new_im)
+	plt.grid('off')
+	plt.axis('off')
 	plt.draw()
 	plt.pause(0.001)
 	return noisyVecs, noisyImages, noises
@@ -300,7 +280,7 @@ def run(experimentNum, num_trials = 20, learning_rate = 15, noise = 0.99, alpha 
 		raw_rankings = [0,0,0,0,0,0]
 		deleted_array = [1,1,1,1,1,1]
 		for rank in range(6,0,-1):
-			print("Input index of image with highest similarity to original image begining with index 1")
+			print("Input index of image with highest similarity to original image beginning with index 1")
 			best_image_index = int(input())
 			raw_rankings[delete_helper(deleted_array, best_image_index)] = rank
 			deleted_array[delete_helper(deleted_array, best_image_index)] = 0
