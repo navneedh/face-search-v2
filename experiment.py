4550c6f8509e235aec23ab0109d41e42f9e2ebd6
navneedh <navneedh@gmail.com>
2019-05-25 18:22:23 -0700

    minor fixes

--- experiment.py
+++ experiment.py
@@ -156,7 +156,7 @@ def present_noise_choices(cur_z, exp_iter, experimentNum, original, cur_reconstr
 		zs = cur_z + noise_val
 		zs = np.clip(zs, -5, 5)
 		p_image = z_sample(Gs, zs)
-		imagesToDisplay.append(np.array(Image.fromarray(p_image).resize((256,256))))
+		imagesToDisplay.append(np.array(Image.fromarray(p_image).resize(size = (256,256), resample = 0)))
 		noises.append(noise_val)
 		noisyVecs.append(zs)
 		noisyImages.append(p_image)
