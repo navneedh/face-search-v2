ff960c6e3407a1ffa163358c30e8e19d4823acb0
Navneedh Maudgalya <navneedhm@berkeley.edu>
2020-05-06 12:20:52 -0700

    minor changes to display

--- experiment.py
+++ experiment.py
@@ -79,7 +79,7 @@ def gen_grid_exp(cur_z, exp_iter, experimentNum, original, cur_reconstructed_ima
 	white_image_fixed = np.array(Image.fromarray(white_image).resize(size = (256,256), resample = False))
 	index = 0
 	print("Generating grid of noisy image proposals ...")
-	print("      1               2              3              4             5              6                         Cur. Reconstruction    Target")
+	print("      1               2              3              4             5              6                   Cur. Reconstruction    Target")
 	for i in range(0,768,128):
 		np.random.seed(np.random.randint(4362634))
 		noise_val = (random_vector() * noise_level) #most noise added
@@ -146,7 +146,7 @@ def present_noise_choices(cur_z, exp_iter, experimentNum, original, cur_reconstr
 	# new_im = Image.new('RGB', (1792,128))
 	white_image_fixed = np.array(Image.fromarray(white_image).resize(size = (256,256), resample = False))
 	index = 0
-	print("   Low Noise Examples                      Medium Noise Examples                 High Noise Examples                    Cur Recon.    Target")
+	print("   Low Noise Examples                      Medium Noise Examples                 High Noise Examples               Cur Recon.    Target")
 	for i in range(0,384,128):
 		noise_val = (random_vector() * 0.85) #least noise added
 		zs = cur_z + noise_val
